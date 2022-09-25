85ba7bcf4def200cde568ec2cb507cf1086a859f
ElvisCA01 <mark.nicols@hotmail.com>
2022-09-25 02:10:48 -0500

    Agregar todo_card

---
+++ src/app/todo-card/todo-card.component.ts
@@ -0,0 +1,48 @@
+import { Component, OnInit } from '@angular/core';
+
+interface Task{
+  done: boolean,
+  tittle: string
+}
+
+@Component({
+  selector: 'app-todo-card',
+  templateUrl: './todo-card.component.html',
+  styleUrls: ['./todo-card.component.scss']
+})
+export class TodoCardComponent implements OnInit {
+
+  title = 'Mis tareas';
+
+  tasks: Array<Task> = [];
+
+  inputTask = 'Nueva tarea';
+
+  constructor() {
+  }
+
+  ngOnInit(): void {
+    this.tasks.push({tittle: "Tarea predeterminado no hecha", done: false});
+    this.tasks.push({tittle: "Tarea predeterminado hecha", done: true});
+
+  }
+
+  ngOnDestroy() {
+
+
+  }
+
+  addTask() {
+    this.tasks.push({tittle: this.inputTask, done: false});
+
+  }
+
+  deleteTask(task: Task) {
+    // TODO(HACER EL ELIMINAR)
+    const index: number = this.tasks.indexOf(task);
+    if(index !== -1){
+      this.tasks.splice(index, 1);
+    }
+    console.log('Eliminando  ${task}')
+  }
+}
